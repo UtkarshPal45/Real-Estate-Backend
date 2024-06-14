253550353002576356308856b3261e296a63c25f
Utkarsh Pal <93117781+UtkarshPal45@users.noreply.github.com>
2024-06-15 03:22:58 +0530

    Update auth-controller.js

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -48,7 +48,6 @@ export const login = async (req,res)=>{
  
         res
         .cookie("token",token,{
-            httpOnly: true, 
             secure: true,
             maxAge: age, 
             path: "/",
