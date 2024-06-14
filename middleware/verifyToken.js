c6cdbe8c153bf228dd2601c694cd62f0e58b272a
Utkarsh Pal <93117781+UtkarshPal45@users.noreply.github.com>
2024-06-15 00:02:41 +0530

    Update verifyToken.js

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 export const verifyToken = (req,res,next)=>{
     //const token = req.cookies.token;
-    console.log(req)
+    console.log(req.cookies)
     
     // if(!token) return res.status(401).json({message: "Not Authenticated"})
 
