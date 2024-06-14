a80fe53db87fa321d56b208f435c609605ec8898
Utkarsh Pal <93117781+UtkarshPal45@users.noreply.github.com>
2024-06-14 23:53:33 +0530

    Update verifyToken.js

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -2,16 +2,16 @@ import jwt from "jsonwebtoken";
 import "dotenv/config";
 
 export const verifyToken = (req,res,next)=>{
-    const token = req.cookies.token;
-    console.log(req.cookies)
+    //const token = req.cookies.token;
+    console.log(req)
     
-    if(!token) return res.status(401).json({message: "Not Authenticated"})
+    // if(!token) return res.status(401).json({message: "Not Authenticated"})
 
-    jwt.verify(token,process.env.JWT_SECURITY_KEY,async(err,payload)=>{
-        if(err) return res.status(403).json({message: "Token is not Valid!"})
+    // jwt.verify(token,process.env.JWT_SECURITY_KEY,async(err,payload)=>{
+    //     if(err) return res.status(403).json({message: "Token is not Valid!"})
     
-        req.userId=payload.id;
-        next()
-    })
+    //     req.userId=payload.id;
+    //     next()
+    // })
 }
 
